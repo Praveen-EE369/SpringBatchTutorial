604bf9083c6732c15f8b035085aa991209819a14
Praveen-EE369 <93862970+Praveen-EE369@users.noreply.github.com>
2022-10-03 19:41:22 +0530

    Soap Rest Web Service

---
+++ TestSoapService/src/main/java/in/TestSoapServiceApplication.java
@@ -0,0 +1,11 @@
+package in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSoapServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TestSoapServiceApplication.class, args);
+	}
+}
